08fb055c473799aae449b3ef3dea264f4ee5f34c
thi4go-git <thi4go19@gmail.com>
2023-11-15 12:56:49 -0300

    Testes NaturezaController 01

--- src/test/java/com/dynss/cloudtecnologia/rest/controller/NaturezaControllerTest.java
+++ src/test/java/com/dynss/cloudtecnologia/rest/controller/NaturezaControllerTest.java
@@ -1,6 +1,7 @@
 package com.dynss.cloudtecnologia.rest.controller;
 
 import com.dynss.cloudtecnologia.rest.dto.NaturezaNewDTO;
+import com.dynss.cloudtecnologia.rest.dto.UsuarioDTO;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.*;
@@ -10,23 +11,25 @@ import javax.ws.rs.core.MediaType;
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
 
+
+
 @QuarkusTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@TestHTTPEndpoint(NaturezaController.class)
 class NaturezaControllerTest {
 
 
     private static final Logger LOG = LoggerFactory.getLogger(UsuarioControllerTest.class);
+    private static final String USERNAME_EXISTENTE = "username2";
+    private static final String TAG_USERNAME = "username";
 
 
     @Test
-    @DisplayName("Chamar Salvar")
+    @DisplayName("Não deve criar uma Natureza com DTO vazio")
     @Order(1)
-    void chamarSalvar() {
+    @TestHTTPEndpoint(NaturezaController.class)
+    void naoCriarNatureza() {
 
         NaturezaNewDTO dto = new NaturezaNewDTO();
-        dto.setDescricao("descrição");
-        dto.setUsername("username");
 
         var resposta = given()
                 .contentType(MediaType.APPLICATION_JSON)
@@ -34,31 +37,62 @@ class NaturezaControllerTest {
                 .when()
                 .post()
                 .then()
-                .extract().response();
+                .extract()
+                .response();
 
         String responseBody = resposta.getBody().asString();
         LOG.info(responseBody);
 
         assertNotNull(responseBody);
+        String mensagemRetorno = resposta.jsonPath().getString("erros");
+
+        assertTrue(mensagemRetorno.contains("campo.descricao.obrigatorio"));
+        assertTrue(mensagemRetorno.contains("campo.username.obrigatorio"));
     }
 
     @Test
-    @DisplayName("obter naturezas por username")
+    @DisplayName("Deve criar uma Natureza com DTO preenchido")
     @Order(2)
-    void deveObter() {
+    void criarNaturezaSucesso() {
+
+        // 1. Cria o usuário primeiro
+        UsuarioDTO dtoUser = new UsuarioDTO();
+        dtoUser.setUsername(USERNAME_EXISTENTE);
+
+        var respostaUser = given()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(dtoUser)
+                .when()
+                .post("/usuarios")
+                .then()
+                .statusCode(201)
+                .extract()
+                .response();
+
+        String usernameCriado = respostaUser.jsonPath().getString(TAG_USERNAME);
+        LOG.info("Usuário criado ::: " + usernameCriado);
+
+
+        // 2. Depois que salvar o usuário, cria a natureza
+        NaturezaNewDTO dtoNatureza = new NaturezaNewDTO();
+        dtoNatureza.setDescricao("Natureza de Exemplo");
+        dtoNatureza.setUsername(usernameCriado);
 
         var resposta = given()
                 .contentType(MediaType.APPLICATION_JSON)
-                .queryParam("username","teste")
+                .body(dtoNatureza)
                 .when()
-                .get()
+                .post("/naturezas")
                 .then()
-                .extract().response();
+                .statusCode(201)
+                .extract()
+                .response();
 
         String responseBody = resposta.getBody().asString();
         LOG.info(responseBody);
 
-        assertNotNull(responseBody);
+        assertEquals(dtoNatureza.getUsername(), resposta.jsonPath().getString(TAG_USERNAME));
+        assertEquals(dtoNatureza.getDescricao(), resposta.jsonPath().getString("descricao"));
     }
 
 
