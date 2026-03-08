fced66acac3ce0ac4bb9a5eec652ea898802ee97
Thiago Melo <thiagomelo@saude-go.net>
2026-03-08 13:17:29 -0300

    Construtor default

--- src/main/java/com/dynss/cloudtecnologia/rest/dto/pluggy/PluggyWebhookDTO.java
+++ src/main/java/com/dynss/cloudtecnologia/rest/dto/pluggy/PluggyWebhookDTO.java
@@ -1,12 +1,16 @@
 package com.dynss.cloudtecnologia.rest.dto.pluggy;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.UUID;
 
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PluggyWebhookDTO {
     private String clientUserId;
     private UUID itemId;
